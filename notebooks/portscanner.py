34d9bded1c162006a60762632e9c5517fed215dc
Benn Aaron Westermann <benn_aaron.westermann@mail.schwarz>
2023-12-11 20:52:43 +0100

    Started to add the Networkscanning feature

--- notebooks/portscanner.py
+++ notebooks/portscanner.py
@@ -1,26 +1,38 @@
 import socket
 import sys
 from datetime import datetime
+import subprocess 
+ip_array=[]
+for ping in range(1,10): 
+    address = "127.0.0." + str(ping) 
+    res = subprocess.call(['ping', '-c', '3', address]) 
+    if res == 0: 
+        ip_array.append(address)
 
-ip = input("Enter the Ip of the Server you want to scan: ")
 
+#ip = input("Enter the Ip of the Server you want to scan: ")
 
 t1 = datetime.now()
-try:
 
-    for port in range(1,65535):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((ip, port))
-        if result == 0:
-            print ("port{}:     open".format(port))
-        sock.close
-except KeyboardInterrupt:
-    sys.exit
+for i in ip_array:
+    try:
+        try:
+    
+            for port in range(1,65535):
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                result = sock.connect_ex((ip_array[i], port))
+            if result == 0:
+                print ("port{}:     open".format(port))
+                sock.close
+        except KeyboardInterrupt:
+            sys.exit
 
-except socket.error:
-    print("invalid IP")
-    sys.exit
+        except socket.error:
+            print("invalid IP")
+            sys.exit
+    except socket.error:
+        sys.exit
+        
 t2 = datetime.now()
 time = t2 - t1
-time = str(time)
-print("It took " + time)
+print(time) 
